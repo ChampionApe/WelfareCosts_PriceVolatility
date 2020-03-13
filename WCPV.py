9e9f1c41afeb89cb623f67082ebc62013b627c76
ChampionApe <rasmus.kehlet.berg@econ.ku.dk>
2020-03-14 00:14:26 +0100

    Update with plot properties

--- WCPV.py
+++ WCPV.py
@@ -1,8 +1,7 @@
-import sys
 from scipy import optimize
 import numpy as np
+import pandas as pd
 import math
-import itertools
 import scipy.stats as stats
 import scipy.integrate as integrate
 import logging
@@ -12,9 +11,7 @@ plt.style.use('seaborn-whitegrid')
 mpl.style.use('seaborn')
 prop_cycle = plt.rcParams["axes.prop_cycle"]
 colors = prop_cycle.by_key()["color"]
-import ipywidgets as widgets
-from ipywidgets import interact, interact_manual
-
+plt.rcParams['font.family'] = 'Palatino Linotype'
 
 logger = logging.getLogger(__name__)
 
@@ -26,10 +23,18 @@ def draw_gh(n,variance,seed=None):
 	temp = stats.truncnorm.rvs(0.5,2,scale=np.sqrt(variance),size=n)
 	return temp/sum(temp)
 
-def draw_ph(n,loc,variance,seed=None):
+def draw_ph(n,loc,variance_p,gh,corr,seed=None):
 	if seed:
 		np.random.seed(seed)
-	return np.random.normal(loc,np.sqrt(variance),n)
+	gh_std = np.sqrt(gh)
+	beta_f = lambda var_p: corr*np.sqrt(var_p)/gh_std
+	eps_std_f= lambda var_p: np.sqrt(var_p-beta_f(var_p)**2*gh_std**2)
+	epsh_f = lambda var_p: np.random.normal(0,eps_std_f(var_p),n)
+	ph_f = lambda var_p: loc+beta_f(var_p)*(gh-1/n)+epsh_f(var_p)
+	eps_std = eps_std_f(variance_p)
+	epsh = epsh_f(variance_p)
+	ph = ph_f(variance_p)
+	return ph,epsh,gh_std,eps_std
 
 def draw_gh_ph(n,loc,variance_p,variance_g,corr,seed=None):
 	if seed:
@@ -45,14 +50,10 @@ def draw_gh_ph(n,loc,variance_p,variance_g,corr,seed=None):
 	ph = ph_f(variance_p)
 	return ph,gh,epsh,gh_std,eps_std
 
-# def update_ph(n,loc,variance_p,corr,gh,gh_std,epsh,varp_new):
-# 	stats.normal.cdf()
-
 class Logit:
 	"""
 	Set up model with logit-like demand function.
 	"""
-
 	def __init__(self,name="",**kwargs):
 		self.name = name
 		self.base_par()
@@ -66,30 +67,41 @@ class Logit:
 		self.lower = 0
 		self.eps = 0 # increase (>=0) for numerical stability of unbounded functions, e.g. log().
 
-		self.n=8760
+		self.sample='ph_gh' # Allowed elements: 'ph','gh','ph_gh', None.
+		self.type='Logit' # allowed elements: Logit, MNL, GNMNL.
+		self.BPRef = 'level' # allowed elements: level, share. In Logit case, only 'level' is allowed. 
+		self.n=500
 		self.loc=200
 		self.variance_p=1
 		self.variance_g=1
-		self.corr=0.1
+		self.corr=0
 		self.seed=30
 		self.utils()
 
 	def utils(self):
 		self.u_cge = lambda C,E: C**(self.alpha)*E**(1-self.alpha)
 		if hasattr(self, 'npv'):
-			self.uhour = lambda E: Logit.staticmethod_uh(self.sigma,self.lower,self.upper,E,self.npv['Eh_l'],self.npv['Eh_u'])
-			self.uh = lambda E: self.uhour(E)-self.uhour(self.npv['Eh'])
-			self.u = lambda C,E: self.u_cge(C,sum(E))+self.uh(E)
-			self.utilde = lambda lambda_: np.exp(self.sigma*lambda_*(self.ph-self.npv['p']))
-			self.grad_utilde = lambda lambda_: self.sigma*(self.ph-self.npv['p'])*self.utilde(lambda_)
-			self.chi = lambda utilde: self.lower+((1-self.lower)*(self.upper-self.lower))/(1-self.lower+(self.upper-1)*utilde)
-			self.grad_chi = lambda utilde: (1-self.lower)*(self.upper-self.lower)*((1-self.lower+(self.upper-1)*utilde))**(-2)*(self.upper-1)
-
+			if self.type=='Logit':
+				self.uhour = lambda E: Logit.staticmethod_uh(self.sigma,self.lower,self.upper,E,self.npv['Eh_l'],self.npv['Eh_u'])
+				self.uh = lambda E: self.uhour(E)-self.uhour(self.npv['Eh'])
+				self.u = lambda C,E: self.u_cge(C,sum(E))+self.uh(E)
+				self.utilde = lambda lambda_: np.exp(self.sigma*lambda_*(self.ph-self.npv['p']))
+				self.grad_utilde = lambda lambda_: self.sigma*(self.ph-self.npv['p'])*self.utilde(lambda_)
+				self.chi = lambda utilde: self.lower+((1-self.lower)*(self.upper-self.lower))/(1-self.lower+(self.upper-1)*utilde)
+				self.grad_chi = lambda utilde: (1-self.lower)*(self.upper-self.lower)*((1-self.lower+(self.upper-1)*utilde))**(-2)*(self.upper-1)
 
 	def upd_par(self,kwargs):
 		for key,value in kwargs.items():
 			setattr(self,key,value)
 		self.utils()
+
+	def upd_all(self,kwargs):
+		self.upd_par(kwargs)
+		self.draw_sample()
+		self.solve_npv()
+		self.solve_fp()
+
+
 
 	@staticmethod
 	def staticmethod_uh(sigma,l,u,E,El,Eu):
@@ -134,8 +146,8 @@ class Logit:
 		self.fp['lambda'],self.fp['C'],self.fp['Eh'] = Logit.x2args(x)
 		self.fp['E'] = sum(self.fp['Eh'])
 		self.fp['p'] = sum(self.ph*self.fp['Eh'])/self.fp['E']
-		self.fp['Eh_l'] = self.fp['Eh']*self.lower-self.eps
-		self.fp['Eh_u'] = self.fp['Eh']*self.upper+self.eps
+		self.fp['Eh_l'] = self.npv['Eh']*self.lower-self.eps
+		self.fp['Eh_u'] = self.npv['Eh']*self.upper+self.eps
 		self.fp['uhour'] = self.uhour(self.fp['Eh'])
 		self.fp['uscale'] = self.npv['uscale']
 		self.fp['u'] = self.u(self.fp['C'],self.fp['Eh'])
@@ -144,41 +156,106 @@ class Logit:
 	def x2args(x):
 		return x[0],x[1],x[2:]
 
-	# @staticmethod
-	# def args2x(sol):
-	# 	return np.append([self.npv['lambda'],self.npv['C']],self.npv['Eh'])
 
-	# Sample functions:
-	def draw_g(self,ph=None):
-		if ph:
-			self.gh = draw_gh(len(ph),self.variance_g,self.seed)
+
+	#################################################################################################
+	#									 PLOTTING PROPERTIES:										#
+	#################################################################################################
+
+	def plots(self,plots):
+		"""
+		Wrapper around plot properties. Plots should be a list of plotting properties. 
+		"""
+		if isinstance(plots,str):
+			fig,axes = plt.subplots(1,1,figsize=(6,4))
+			plt.subplot(1,1,1)
+			eval('self.'+plots)
 		else:
+			if len(plots)==1:
+				fig,axes = plt.subplots(1,1,figsize=(6,4))
+				plt.subplot(1,1,1)
+				eval('self.'+plots[0])
+			else:
+				fig,axes = plt.subplots(round(len(plots)/2),2,figsize=(12,4*round(len(plots)/2)))
+				for i in range(len(plots)):
+					plt.subplot(round(len(plots)/2),2,i+1)
+					eval('self.'+plots[i])
+		fig.tight_layout()
+
+	@property 
+	def plt_compare_eh(self):
+		plt.plot(pd.Series(self.npv['Eh']).sort_values().reset_index(drop=True))
+		plt.plot(pd.Series(self.fp['Eh']).sort_values().reset_index(drop=True))
+		plt.xlabel('Hours of the year (sorted)', fontsize=13)
+		plt.ylabel('$E_h$', fontsize=13)
+		plt.legend(('$E_h$, constant prices', '$E_h$, flexible prices'), fontsize=13)
+		plt.title('Sorted hourly consumption with/without flexible prices', fontweight='bold',fontsize=16)
+
+	@property
+	def plt_ph(self):
+		plt.plot(pd.Series(self.ph).sort_values().reset_index(drop=True))
+		plt.xlabel('Hours of the year (sorted)', fontsize=13)
+		plt.ylabel(('Hourly prices'), fontsize=13)
+		plt.title('Sorted hourly prices', fontweight='bold',fontsize=16)
+
+	@property 
+	def plt_gh(self):
+		plt.plot(pd.Series(self.gh).sort_values().reset_index(drop=True))
+		plt.xlabel('Hours of the year (sorted)', fontsize=13)
+		plt.ylabel(('Preferences $g_h$'), fontsize=13)
+		plt.title('Sorted hourly preferences', fontweight='bold',fontsize=16)
+
+	@property
+	def plt_eh_npv_gh(self):
+		plt.scatter(self.npv['Eh'],self.gh)
+		plt.xlabel('$E_h$ without flexible prices',fontsize=13)
+		plt.ylabel('Preferences $g_h$',fontsize=13)
+		plt.title('Hourly consumption/preferences (npv)', fontweight='bold', fontsize=16)
+
+	@property
+	def plt_eh_npv_ph(self):
+		plt.scatter(self.npv['Eh'],self.ph)
+		plt.xlabel('$E_h$ without flexible prices',fontsize=13)
+		plt.ylabel('Hourly prices $p_h$',fontsize=13)
+		plt.title('Hourly consumption/prices (npv)', fontweight='bold', fontsize=16)
+
+	@property
+	def plt_eh_fp_gh(self):
+		plt.scatter(self.fp['Eh'],self.gh)
+		plt.xlabel('$E_h$, flexible prices',fontsize=13)
+		plt.ylabel('Preferences $g_h$',fontsize=13)
+		plt.title('Hourly consumption/preferences (fp)', fontweight='bold', fontsize=16)
+
+	@property
+	def plt_eh_fp_ph(self):
+		plt.scatter(self.fp['Eh'],self.ph)
+		plt.xlabel('$E_h$, flexible prices',fontsize=13)
+		plt.ylabel('Hourly prices $p_h$',fontsize=13)
+		plt.title('Hourly consumption/prices (fp)', fontweight='bold', fontsize=16)
+
+	@property
+	def plt_uhour(self):
+		ehgrid = np.linspace(self.npv['Eh_l'][0]+0.00001,self.npv['Eh_u'][0]-0.00001,100)
+		uh = (1/self.sigma)*(ehgrid * np.log((1-self.lower)/(self.upper-1))-(ehgrid-self.npv['Eh_l'][0])*np.log(ehgrid-self.npv['Eh_l'][0])-(self.npv['Eh_u'][0]-ehgrid)*np.log(self.npv['Eh_u'][0]-ehgrid))
+		plt.plot(ehgrid,uh)
+		plt.axvline(x=self.gh[0]*self.npv['E'], color='k', linestyle='--', label='$g_hE_t^*$')
+		plt.xlabel('$E_h$',fontsize=13)
+		plt.ylabel('$u^{hour}_h$',fontsize=13)
+		plt.legend(('$u_h^{hour}$', '$g_hE_t^*$'))
+		plt.title('Hourly utility component', fontweight='bold',fontsize=16)
+
+	#################################################################################################
+	#									 	SAMPLING METHODS:										#
+	#################################################################################################
+
+	def draw_sample(self):
+		if self.sample=='ph_gh':
+			self.ph,self.gh,self.epsh,self.gh_std,self.eps_std = draw_gh_ph(self.n,self.loc,self.variance_p,self.variance_g,self.corr,self.seed)
+		elif self.sample=='ph':
+			self.ph,self.epsh,self.gh_std,self.eps_std = draw_ph(self.n,self.loc,self.variance_p,self.gh,self.corr,self.seed)
+		elif self.sample=='gh':
 			self.gh = draw_gh(self.n,self.variance_g,self.seed)
-
-	def draw_p(self,gh=None):
-		if gh:
-			self.ph = draw_ph(len(gh),self.loc,self.variance_p,self.seed)
-		else:
-			self.ph = draw_ph(self.n,self.loc,self.variance_p,self.seed)
-
-	def sample(self):
-		self.ph,self.gh,self.epsh,self.gh_std,self.eps_std = draw_gh_ph(self.n,self.loc,self.variance_p,self.variance_g,self.corr,self.seed)
 		self.p = sum(self.ph*self.gh)
-
-	# Mean-preserving increase in volatility on existing sample:
-	# @staticmethod
-	# def linear_perturb(ph,a,p):
-	# 	return ph*(1+(ph>p)*a-(ph<p)*a)
-
-	# @staticmethod
-	# def wp(ph,gh,a,p):
-	# 	return sum(gh*Logit.linear_perturb(ph,a,p))
-
-	# @staticmethod
-	# def sample_perturbation(ph,gh,p,n,target_variance,x0=0):
-	# 	def f(x):
-	# 		return target_variance-sum((Logit.linear_perturb(ph,x,p)-Logit.wp(ph,gh,x,p))**2)/n
-	# 	return optimize.fsolve(f,x0)
 
 	def perturbed_samples(self,vec_variance):
 		try:
